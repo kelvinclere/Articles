894c8a31f223eb7dc1e2eee5726b5db03e193b59
kelvinclere <kelvinmutethiamuthomi@gmail.com>
2022-10-31 11:20:33 +0300

    done

--- tools/console.rb
+++ tools/console.rb
@@ -7,21 +7,21 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 # Magazine
-mag1 = Magazine.new("nation", "political")
+mag1 = Magazine.new("Relationship", "political")
 mag2 = Magazine.new("star", "business")
-mag3 = Magazine.new("standard", "editorial")
+mag3 = Magazine.new("Daily nation", "Parenting")
 
 #Author
-author1 = Author.new("John")
-author2 = Author.new("Peter")
-author3 = Author.new("Kevin")
+author1 = Author.new("Wizzy")
+author2 = Author.new("Kelvin")
+author3 = Author.new("Anna")
 
 #Articles
-art1 = Article.new(author1, mag1, "Elections 2022")
+art1 = Article.new(author1, mag1, "Food crisis")
 art2 = Article.new(author2, mag2, "Mombase Port")
-art3 = Article.new(author1, mag1, "DJ Trump")
-art4 = Article.new(author1, mag3, "Ukraine War Crisis")
-art5 = Article.new(author3, mag3, "Standard's 2nd author")
+art3 = Article.new(author1, mag1, "Sports")
+art4 = Article.new(author1, mag3, "CS vetting")
+art5 = Article.new(author3, mag3, "The founder of wilson Airport")
 
 
 
